d7f936f3e4c12059f889f5f77fdcf95615e5ef4c
chengzh2008 <chengzh2008@gmail.com>
2015-04-24 11:20:41 -0700

    Best time to buy and sell stock

---
+++ src/src/main/java/BestTimeBuySellStock.java
@@ -0,0 +1,25 @@
+package src.main.java;
+
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * <p>
+ * If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock),
+ * design an algorithm to find the maximum profit.
+ */
+public class BestTimeBuySellStock {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (prices == null || n <= 1) {
+            return 0;
+        }
+        int[] profit = new int[n];
+        int[] lowestPriceSoFar = new int[n];
+        lowestPriceSoFar[0] = prices[0];
+        // current max profit is the maximum of previous max and the current price - lowest price so far
+        for (int i = 1; i < n; i++) {
+            profit[i] = Math.max(profit[i - 1], prices[i] - lowestPriceSoFar[i - 1]);
+            lowestPriceSoFar[i] = Math.min(lowestPriceSoFar[i - 1], prices[i]);
+        }
+        return profit[n - 1];
+    }
+}
